1fc861d8858124415c97c83456bcf2e3836c70ec
GitHub <noreply@github.com>
2021-10-19 01:05:08 +0000

    :package: Update deno dependencies

--- denops/@denops/deps_test.ts
+++ denops/@denops/deps_test.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.109.0/testing/asserts.ts";
-export { deadline, deferred } from "https://deno.land/std@0.109.0/async/mod.ts";
+export * from "https://deno.land/std@0.112.0/testing/asserts.ts";
+export { deadline, deferred } from "https://deno.land/std@0.112.0/async/mod.ts";
